382ec64493ebfa8539b8df36fa93fabf1c216b20
mochi7054 <229789565+mochi7054@users.noreply.github.com>
2026-06-25 14:19:50 +0900

    Add details to TrashCubeBlock tooltip using TrashCubeBlockItem

---
+++ src/main/java/com/github/mochi7054/trashcube/item/TrashCubeBlockItem.java
@@ -0,0 +1,11 @@
+package com.github.mochi7054.trashcube.item;
+
+import com.github.mochi7054.trashcube.block.TrashCubeBlock;
+import mekanism.common.item.block.ItemBlockTooltip;
+import net.minecraft.world.item.Item;
+
+public class TrashCubeBlockItem extends ItemBlockTooltip<TrashCubeBlock> {
+    public TrashCubeBlockItem(TrashCubeBlock block, Item.Properties properties) {
+        super(block, true, properties);
+    }
+}
