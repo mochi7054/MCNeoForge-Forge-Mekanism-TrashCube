5fea6f27c5703f87862da8f2216086418489163b
mochi7054 <229789565+mochi7054@users.noreply.github.com>
2026-06-25 14:14:12 +0900

    Update Radioactive Upgrade recipe, max stack size to 64, and require correct tool for drops on TrashCube

--- src/main/java/com/github/mochi7054/trashcube/TrashCube.java
+++ src/main/java/com/github/mochi7054/trashcube/TrashCube.java
@@ -29,14 +29,14 @@ public class TrashCube {
 
     public static final mekanism.common.registration.impl.BlockRegistryObject<TrashCubeBlock, mekanism.common.item.block.ItemBlockTooltip> BLOCK =
             BLOCKS.register("trash_cube",
-                    () -> new TrashCubeBlock(BlockBehaviour.Properties.of().strength(3.5F).noOcclusion()),
+                    () -> new TrashCubeBlock(BlockBehaviour.Properties.of().strength(3.5F).requiresCorrectToolForDrops().noOcclusion()),
                     mekanism.common.item.block.ItemBlockTooltip::new);
 
     public static Upgrade RADIOACTIVE_UPGRADE_TYPE;
 
     public static final DeferredItem<RadioactiveUpgradeItem> RADIOACTIVE_UPGRADE =
             ITEMS.register("radioactive_upgrade",
-                    () -> new RadioactiveUpgradeItem(new Item.Properties().stacksTo(1)));
+                    () -> new RadioactiveUpgradeItem(new Item.Properties().stacksTo(64)));
 
     public static final mekanism.common.registration.impl.TileEntityTypeRegistryObject<TrashCubeBlockEntity> TILE =
             BLOCK_ENTITIES.mekBuilder(BLOCK, TrashCubeBlockEntity::new)
