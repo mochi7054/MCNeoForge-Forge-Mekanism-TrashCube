5dcca6d25018a02cde743b072d7d1e9217b70ffb
みちゃもち <229789565+mochi7054@users.noreply.github.com>
2026-09-10 08:25:25 +0900

    Fix radioactive upgrade tooltip by delegating to ItemUpgrade

--- src/main/java/com/github/mochi7054/trashcube/item/RadioactiveUpgradeItem.java
+++ src/main/java/com/github/mochi7054/trashcube/item/RadioactiveUpgradeItem.java
@@ -1,18 +1,8 @@
 package com.github.mochi7054.trashcube.item;
 
-import java.util.List;
+import com.github.mochi7054.trashcube.TrashCube;
 import mekanism.api.Upgrade;
-import mekanism.api.text.EnumColor;
-import mekanism.client.key.MekKeyHandler;
-import mekanism.client.key.MekanismKeyHandler;
-import mekanism.common.MekanismLang;
 import mekanism.common.item.ItemUpgrade;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class RadioactiveUpgradeItem extends ItemUpgrade {
 
@@ -22,19 +12,6 @@ public class RadioactiveUpgradeItem extends ItemUpgrade {
 
     private static Upgrade getUpgradeTypeForceLoad() {
         Upgrade.values();
-        return com.github.mochi7054.trashcube.TrashCube.RADIOACTIVE_UPGRADE_TYPE;
-    }
-
-    @Override
-    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level world, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag) {
-        if (MekKeyHandler.isKeyPressed(MekanismKeyHandler.detailsKey)) {
-            tooltip.add(Component.translatable("upgrade.mekanismtrashcube.radioactive.desc"));
-            tooltip.add(Component.translatable("mekanism.gui.upgrade.max_installed", 1));
-        } else {
-            tooltip.add(
-               MekanismLang.HOLD_FOR_DETAILS
-                  .translateColored(EnumColor.GRAY, new Object[]{EnumColor.INDIGO, MekanismKeyHandler.detailsKey.getTranslatedKeyMessage()})
-            );
-        }
+        return TrashCube.RADIOACTIVE_UPGRADE_TYPE;
     }
 }
